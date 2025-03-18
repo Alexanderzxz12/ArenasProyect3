55eb19aa5107604fc31b779c057e05740985ebdc
UserMachineWork <jhoalexxxcc@gmail.com>
2025-03-18 17:37:51 -0500

    prueba de git

--- ArenasProyect3/Conexion/ConexionMaestra.cs
+++ ArenasProyect3/Conexion/ConexionMaestra.cs
@@ -13,6 +13,7 @@ namespace ArenasProyect3.Conexion
         //public static string conexion = @"Server = tcp:LAPTOP-JCUADROS\SQLEXPRESS,49500;DataBase=BD_VENTAS_2;User = sa;Password=12345";
         //connectionString="Data Source=192.168.1.203\SA,1433;Initial Catalog=dbarenasprod;User ID=sa;Password=Arenas2019"
         public static string conexion = @"Server = tcp:192.168.1.154,1433;DataBase=BD_VENTAS_2;User = sa;Password=Arenas.2020!";
+        //PRUEBA DE GIT
     }
 
 }
