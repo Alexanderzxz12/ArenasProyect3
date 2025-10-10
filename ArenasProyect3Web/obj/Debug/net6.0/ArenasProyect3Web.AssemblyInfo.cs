dba0262100c3be88b9fc56e6b041790a6de3265a
Alexander <jhoalexxxcc@gmail.com>
2025-10-10 08:55:31 -0500

    Todo conforme, merge desecho

--- ArenasProyect3Web/obj/Debug/net6.0/ArenasProyect3Web.AssemblyInfo.cs
+++ ArenasProyect3Web/obj/Debug/net6.0/ArenasProyect3Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArenasProyect3Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cb247de29f6129ef79d3ed51c748b22e9a0e05b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+867c6a2c590e5753d348c2dba8ca259711f22caf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArenasProyect3Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArenasProyect3Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
